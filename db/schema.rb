7d854b83616b163d0a92e111a4efabaeb8fe5786
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-17 15:41:19 +0900

    カラムの型_修正

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 2023_01_15_020013) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", null: false
     t.string "second_name", null: false
     t.string "first_name_kana", null: false
     t.string "second_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
