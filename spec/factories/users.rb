9b317e391c483a30ecfe52740dff4693f43261b9
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-18 00:17:53 +0900

    Faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'abc'}
-    email                 {'kkk@gmail.com'}
-    password              {'abc00000'}
+    nickname              {Faker::Name.initials(number: 3)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    first_name            {"山田"}
-    second_name           {"太郎"}
-    first_name_kana       {"ヤマダ"}
-    second_name_kana      {"タロウ"}
-    birthday              {"1111"}
+    first_name            {Faker::Name.last_name}
+    second_name           {Faker::Name.first_name}
+    first_name_kana       {Faker::Name.last_name}
+    second_name_kana      {Faker::Name.first_name}
+    birthday              {Faker::Number.number(8)}
   end
 end
