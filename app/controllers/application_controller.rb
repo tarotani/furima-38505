5d20eb0a1287ce86bd1fecc248efe1d348a8fa60
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-17 15:52:20 +0900

    新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
 
@@ -10,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sigh_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday])
   end
 end
