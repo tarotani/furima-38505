c49f219af5ad3028539720704e522bab2a816f1e
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-19 09:54:37 +0900

    Faker_password_修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 3)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"test1234"}
     password_confirmation {password}
     first_name            {Faker::Name.last_name}
     second_name           {Faker::Name.first_name}
