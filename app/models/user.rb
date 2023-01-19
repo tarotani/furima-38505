f285fffc9ec2623c304ac98dc1cae5d4aa654a34
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-19 13:22:39 +0900

    バリデーション_修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,15 @@ class User < ApplicationRecord
   #has_many :orders
 
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
-  validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :second_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i} 
+  validates :first_name, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
+  validates :second_name, presence: true
+  validates :second_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
+  validates :first_name_kana, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :second_name_kana, presence: true
+  validates :second_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
 
