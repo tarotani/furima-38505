3748a6f23bfc3fb3fadd4732eafc6cb14f1f643d
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-19 13:21:40 +0900

    Faker_修正２

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 3)}
+    nickname              {"test"}
     email                 {Faker::Internet.free_email}
     password              {"test1234"}
     password_confirmation {password}
-    first_name            {Faker::Name.last_name}
-    second_name           {Faker::Name.first_name}
-    first_name_kana       {Faker::Name.initials(number: 4)}
-    second_name_kana      {Faker::Name.initials(number: 4)}
-    birthday              {Faker::DATE.birthday}
+    first_name            {"山田"}
+    second_name           {"太郎"}
+    first_name_kana       {"ヤマダ"}
+    second_name_kana      {"タロウ"}
+    birthday              {"1970-01-01"}
   end
 end
