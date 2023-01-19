695a75b85bfb06f84a9c3ae324fa8c41f1a7a4c2
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-19 11:54:56 +0900

    バリデーション（パスワード確認用）_追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
   validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
