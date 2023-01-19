5e32d4140a5746429acb9e62ecbd6592be5c3a50
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-19 12:08:55 +0900

    Faker_修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     password_confirmation {password}
     first_name            {Faker::Name.last_name}
     second_name           {Faker::Name.first_name}
-    first_name_kana       {Faker::Name.last_name}
-    second_name_kana      {Faker::Name.first_name}
-    birthday              {Faker::Number.number(8)}
+    first_name_kana       {Faker::Name.initials(number: 4)}
+    second_name_kana      {Faker::Name.initials(number: 4)}
+    birthday              {Faker::DATE.birthday}
   end
 end
