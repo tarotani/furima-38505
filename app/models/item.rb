91498f4a2399fb88c3a60d3ff862dbe96d0be99a
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-22 02:16:49 +0900

    バリデーション_追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,22 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :conditions
+  belongs_to :postages
+  belongs_to :firsts_add
+  belongs_to :sends_day
 
+  validates :name, presence: true
+  validates :text, presence: true
   validates :category_id, presence: true
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, presence: true
+  validates :conditions_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :postage_id, presence: true
+  validates :postages_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :first_add_id, presence: true
+  validates :firsts_add_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :send_day_id, presence: true
+  validates :sends_day_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
