42fa6375e4d77913756f717d840539b77e897241
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-21 16:10:28 +0900

    itemsモデルとテーブル作成

--- db/migrate/20221231085008_create_items.rb
+++ db/migrate/20221231085008_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string     :name,           null: false
+      t.text       :text,           null: false
+      t.integer    :price,          null: false
+      t.integer    :condition_id,   null: false
+      t.integer    :postage_id,     null: false
+      t.integer    :first_add_id,   null: false
+      t.integer    :send_day_id,    null: false
+      t.integer    :category_id,    null: false
+      t.references :user,           null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_020013) do
+ActiveRecord::Schema.define(version: 2022_12_31_085008) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.integer "price", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "first_add_id", null: false
+    t.integer "send_day_id", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", null: false
     t.string "second_name", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2023_01_15_020013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
