095b656d408e0f1c0fef36a0183f6655825d918b
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-21 17:47:13 +0900

    バリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
-  
+    @items = Item.order("created_at DESC")
   end
+
+  #def new
+  #end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :category_id, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
