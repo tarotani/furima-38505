039df08ef30b3a23ffa7ab5799e06c8dc152070a
tarotani <115441407+tarotani@users.noreply.github.com>
2023-01-29 18:29:29 +0900

    バリデーション_修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,29 @@
 class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
-  belongs_to :conditions
-  belongs_to :postages
-  belongs_to :firsts_add
-  belongs_to :sends_day
 
+  belongs_to :category__id, optional: true
+  belongs_to :condition__id, optional: true
+  belongs_to :postage__id, optional: true
+  belongs_to :first_add__id, optional: true
+  belongs_to :send_day__id, optional: true
+
+  #belongs_to :user, optional: true
+  #has_one :order
+
+  validates :image, presence: true
   validates :name, presence: true
   validates :text, presence: true
   validates :category_id, presence: true
-  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :condition_id, presence: true
-  validates :conditions_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :postage_id, presence: true
-  validates :postages_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :first_add_id, presence: true
-  validates :firsts_add_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :first_add_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :send_day_id, presence: true
-  validates :sends_day_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_9999_999}
