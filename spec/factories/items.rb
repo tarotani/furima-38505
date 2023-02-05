d8f2dd7eb513a2737b4b825cecf6ec4fa03d5af6
tarotani <115441407+tarotani@users.noreply.github.com>
2023-02-05 19:04:44 +0900

    spec/factories/items.rb_修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,11 +7,13 @@ FactoryBot.define do
     postage_id        {2}
     first_add_id      {2}
     send_day_id       {2}
-    category_id       {2}              
+    category_id       {2}
+    user
+    after(:build) do |message|
+      message.image.attach(io: File.open("public/images/image.png"), filename: "image.png")
+    end              
   end
 
-  after(:build) do |message|
-    message.image.attach(io: File.open("public/images/image.png"), filename: "image.png")
-  end
+
 
 end
