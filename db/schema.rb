289367be6059265190d4b59f973698e487afebce
tarotani <115441407+tarotani@users.noreply.github.com>
2023-02-05 23:31:03 +0900

    db/schema.rb_修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 2023_01_21_071652) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "text", null: false
@@ -72,5 +67,5 @@ ActiveRecord::Schema.define(version: 2023_01_21_071652) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "items", "users"
 end
