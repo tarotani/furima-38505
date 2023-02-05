428849c2c672bece25d428cdd660bd2c7c8b4c26
tarotani <115441407+tarotani@users.noreply.github.com>
2023-02-05 23:02:21 +0900

    マイグレーション_修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 2023_01_21_071652) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "text", null: false
