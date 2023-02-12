6add3e3cdc592faa78115fba62f7c661bba3c942
tarotani <115441407+tarotani@users.noreply.github.com>
2023-02-12 16:36:44 +0900

    重複した処理をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,12 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
     else
       redirect_to root_path
@@ -32,7 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     # バリデーションがOKであれば詳細画面へ
     if @item.valid?
@@ -47,6 +46,10 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:image, :name, :text, :price, :category_id, :condition_id, :postage_id, :first_add_id, :send_day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end  
 
 end
