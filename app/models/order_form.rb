b0cee354f0c4e5da8ca6c76c0282ba1447436ef9
tarotani <115441407+tarotani@users.noreply.github.com>
2023-02-18 22:53:19 +0900

    オーダーフォーム（トークン追記）

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,6 +1,6 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postalcode, :first_add_id, :second_add, :address, :building, :tel#, :token
+  attr_accessor :user_id, :item_id, :postalcode, :first_add_id, :second_add, :address, :building, :tel, :token
 
   with_options presence: true do
     # orderモデルのバリデーション
@@ -13,12 +13,13 @@ class OrderForm
     validates :address
     validates :tel, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
     # トークンのバリデーション
-    #validates :token
+    validates :token
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     # ストロングパラメーターでデータが運ばれ、それらが保存のタイミングで「order_id」が生成され、保存される。
-    Ship.create(postalcode: postalcode, first_add_id: first_add_id, second_add: second_add, address: address,building: building, tel: tel, order_id: order.id)
+    Ship.create(postalcode: postalcode, first_add_id: first_add_id, second_add: second_add, address: address, building: building,
+       tel: tel, order_id: order.id)
   end
 end
